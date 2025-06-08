2c405e31debfa9a31ead1595df97deca97741759
Priyanshu kumar <priyanshu61iitj@gmail.com>
2025-06-09 00:00:46 +0530

    fix: update MongoDB connection string to use placeholder values

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use(cors());
 // Database connection
 mongoose.connect(
   process.env.MONGODB_URI ||
-    "mongodb+srv://priyanshu61iitj:ILdvydpdEY777K74@cluster0.h5rlkig.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "mongodb+srv://username:password@cluster0.h5rlkig.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
